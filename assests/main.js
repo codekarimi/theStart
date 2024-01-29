c34bd1c766a78b7f0a90d3437d777af1802dbd68
zeuss254 <ckarimi676@gmail.com>
2024-01-29 12:40:31 +0300

    folder with styling and code for calculator functionality

---
+++ assests/main.js
@@ -0,0 +1,46 @@
+
+// FUNCTION TO APPEND VALUE TO INPUT 
+// function calculate(value) {
+//   document.getElementById('result').value += value;
+// }
+
+// function evaluate() {
+//   let display = document.getElementById('result').value;
+//   let answer = eval(display);
+
+// //   document.getElementById('result').value = answer;
+
+// return answer;
+
+// }
+
+// const answer = document.getElementById("click");
+// answer.addEventListener('click',evaluate)
+
+// // let displayValue = '';
+
+// // function updateDisplay() {
+// //     document.getElementById('result').value = displayValue;
+// // }
+
+
+// // function evaluate() {
+// //   let result = document.getElementById('result').value;
+// //   let answer;
+// //   try {
+// //     answer = eval(result);
+// //     if (answer === undefined || isNaN(answer)) {
+// //       throw new Error('Invalid input');
+// //     }
+// //     document.getElementById('result').value = answer;
+// //   } catch (error) {
+// //     document.getElementById('result').value = 'Error';
+// //   }
+// // }
+
+// // FUNCTION TO CLEAR VALUE FROM INPUT
+// function clearResult() {
+//   document.getElementById('result').value = '';
+// }
+
+
